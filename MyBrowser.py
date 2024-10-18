6801f1a1cde008b28857868e1ee2dc8cdb3151cf
Raghavnadiminti <raghavnadiminti@gmail.com>
2024-10-18 18:34:21 +0530

    added

--- MyBrowser.py
+++ MyBrowser.py
@@ -1,6 +1,7 @@
 import sys 
 from PyQt6.QtWidgets import * 
-
+from PyQt6.QtWebEngineWidgets import *
+from PyQt6.QtCore import *
 
 class InputBar(QWidget):
     def __init__(self):
@@ -10,14 +11,35 @@ class InputBar(QWidget):
     
     def Window1(self):
         self.setWindowTitle("MY Browser")
+        self.mainlayout=QVBoxLayout()
         self.layout=QHBoxLayout()
         # self.resize(400, 200) 
+        self.layout2=QVBoxLayout()
         self.button=QPushButton("enter")
+        
         self.layout.addWidget(self.button)
         self.line_edit=QLineEdit()
+        self.button.clicked.connect(self.getinput) 
+        self.browser=QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.google.com"))
         self.layout.addWidget(self.line_edit)
         self.layout.addStretch()
-        self.setLayout(self.layout)
+        self.layout2.addWidget(self.browser)
+       
+        self.layout2.addStretch()
+        self.mainlayout.addLayout(self.layout)
+        self.mainlayout.addLayout(self.layout2)
+        self.mainlayout.setStretch(0, 1)
+        self.mainlayout.setStretch(1, 10)
+        
+        self.setLayout(self.mainlayout)
+        self.showMaximized()
+        
+    def getinput(self):
+        input=self.line_edit.text()
+        if input:
+            self.browser.setUrl(QUrl(input))
+    
     
 class   MyWindow(QWidget):
     def __init__(self):
@@ -26,9 +48,21 @@ class   MyWindow(QWidget):
     def showWindow(self):
         self.inptBar=InputBar() 
         self.layout=QVBoxLayout()
+       
+        
         self.layout.addWidget(self.inptBar)
+        
+        
+       
         self.layout.addStretch()
         self.setLayout(self.layout)
+    
+
+        
+        
+        
+    
+
 
 
 
